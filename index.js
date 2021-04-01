b3e91c2bfe268df87d2aa7c397027cabbded0c2c
Malena de Juan <malena.dj4@gmail.com>
2021-04-01 15:56:16 +0200

    errores con métodos put, post y delete

--- index.js
+++ index.js
@@ -21,10 +21,22 @@ app.get("/metro/lineas", (req, res, next) => {
 app.get("/metro/linea", (req, res, next) => {
   res.send("linea");
 });
+app.get("/metro/lineas", (req, res, next) => {
+  res.send("lineas");
+});
+app.put("/:parametro?", (req, res, next) => {
+  res.status(403).json({ error: true, mensaje: "Te pensabas que podías hackerme" });
+});
+app.post("/:parametro?", (req, res, next) => {
+  res.status(403).json({ error: true, mensaje: "Te pensabas que podías hackerme" });
+});
+app.delete("/:parametro?", (req, res, next) => {
+  res.status(403).json({ error: true, mensaje: "Te pensabas que podías hackerme" });
+});
 app.use((req, res, next) => {
   res.status(404).json({ error: true, mensaje: "Recurso no encontrado" });
 });
-app.use((err, res, req, next) => {
+app.use((err, req, res, next) => {
   debug(err);
   res.status(500).json({ error: true, mensaje: "Error general" });
 })
